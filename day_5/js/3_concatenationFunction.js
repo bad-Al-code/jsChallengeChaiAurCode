59da8a64d4c860df3c3e07c436e65f3445fd7768
badAl <badalsingh120403@gmail.com>
2024-07-17 08:25:20 +0530

    feat: Add Day 5 JavaScript challenge scripts for functions
    - Added `1_evenOrOdd.js` to check if a number is even or odd.
    - Added `2_squareCalculation.js` to calculate the square of a number.
    - Added `3_concatenationFunction.js` to concatenate two strings.
    - Added `4_sumCalculationArrowFunction.js` to calculate the sum of two
    numbers using an arrow function.
    - Added `5_highOrderFunctions.js` to demonstrate higher-order functions.

---
+++ day_5/js/3_concatenationFunction.js
@@ -0,0 +1,6 @@
+const concatenateStrings = function (str1, str2) {
+  return str1 + str2;
+};
+
+console.log(concatenateStrings("Hello, ", "Al!"));
+console.log(concatenateStrings("IDK, why ", "I'm here"));
