a88b0895a64eeb1a527724a868fd673535553d28
badAl <badalsingh120403@gmail.com>
2024-08-05 10:42:26 +0530

    Add fetch functionality to get current weather data and log it to the console

--- day_24/script.js
+++ day_24/script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Weather App");
+  const apiKey = "73d99aea1fcc471daad50158240508";
+  const city = "Delhi";
+  const weatherApiURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  //   console.log(weatherApiURL);
+
+  async function fetchWeatherData(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching weather data: ", error);
+    }
+  }
+
+  fetchWeatherData(weatherApiURL);
 });
