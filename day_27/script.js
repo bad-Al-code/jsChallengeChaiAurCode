868d716a33cea93d6b1483ed1c214d267a51ccf1
badAl <badalsingh120403@gmail.com>
2024-08-12 09:52:02 +0530

    feat: add edit button to task items and implemet its editing functionality

--- day_27/script.js
+++ day_27/script.js
@@ -33,9 +33,24 @@ function displayTasks() {
             <h3>${task.title}</h3>
             <p>${task.description}</p>
             <small>Due Date: ${task.dueDate}</small>
+            <button class="edit-task" onclick="editTask(${task.id})">Edit</button>
         `;
     taskList.appendChild(taskItem);
   });
+}
+
+function editTask(taskId) {
+  const taskToEdit = tasks.find((task) => task.id === taskId);
+
+  if (taskToEdit) {
+    document.getElementById("task-title").value = taskToEdit.title;
+    document.getElementById("task-desc").value = taskToEdit.description;
+    document.getElementById("task-due-date").value = taskToEdit.dueDate;
+
+    document
+      .getElementById("task-form")
+      .setAttribute("data-editing-task-id", taskId);
+  }
 }
 
 document
